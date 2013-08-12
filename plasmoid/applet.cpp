566ea3b344b676198ac2a46e3751d726af615b42
Vishesh Handa <me@vhanda.in>
2013-08-12 18:31:21 +0530

    Convert it to a Plasma::Applet with a DeclarativeWidget
    It needs to be a C++ Applet if we want a proper configuration dialog

---
+++ plasmoid/applet.cpp
@@ -0,0 +1,52 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright 2013  Vishesh Handa <me@vhanda.in>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "applet.h"
+
+#include <KGlobal>
+#include <KStandardDirs>
+
+Applet::Applet(QObject* parent, const QVariantList& args): PopupApplet(parent, args)
+{
+    setPopupIcon("nepomuk");
+}
+
+Applet::~Applet()
+{
+}
+
+void Applet::init()
+{
+    m_declarativeWidget = new Plasma::DeclarativeWidget(this);
+
+    QString qmlFile = KGlobal::dirs()->findResource("data", "plasma/plasmoids/org.kde.nepomuk.finder/contents/ui/main.qml");
+    m_declarativeWidget->setQmlPath(qmlFile);
+
+    Plasma::Applet::init();
+}
+
+QGraphicsWidget* Applet::graphicsWidget()
+{
+    return m_declarativeWidget;
+}
+
+#include "applet.moc"
