5f0c6baae039a03e80b76c0036847bfbd21ddc5d
Vishesh Handa <me@vhanda.in>
2013-09-04 15:31:32 +0530

    ApplicationSource: Add more places to search from
    Code adapted from plasma service runner
    
    The results are a lot better now.

--- lib/applicationsource.cpp
+++ lib/applicationsource.cpp
@@ -2,6 +2,9 @@
  * <one line to give the library's name and an idea of what it does.>
  * Copyright 2013  Vishesh Handa <me@vhanda.in>
  *
+ * Code adapted from kde-workspace/plasma/generic/runners/servicerunner.cpp
+ * Copyright (C) 2006 Aaron Seigo <aseigo@kde.org>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
@@ -25,9 +28,11 @@
 #include <KService>
 #include <KServiceTypeTrader>
 #include <KRun>
+#include <QSet>
 
 ApplicationSource::ApplicationSource(QObject* parent): AbstractSource(parent)
 {
+    //TODO: Find an applications icon!
     m_applicationType = new MatchType(i18n("Applications"), "bah");
     m_kcmType = new MatchType(i18n("System Settings"), "preferences-desktop");
 
@@ -37,12 +42,13 @@ ApplicationSource::ApplicationSource(QObject* parent): AbstractSource(parent)
     setTypes(types);
 }
 
-void ApplicationSource::query(const QString& string)
+void ApplicationSource::query(const QString& term)
 {
-    if (string.size() < 3)
+    if (!term.size())
         return;
 
-    QString queryStr = QString("exist Exec and ('%1' ~~ Name)").arg(string);
+    QSet<QString> seen;
+    QString queryStr = QString("exist Exec and ('%1' ~~ Name)").arg(term);
 
     if (m_applicationType->isShown()) {
         KService::List services = KServiceTypeTrader::self()->query("Application", queryStr);
@@ -58,6 +64,8 @@ void ApplicationSource::query(const QString& string)
             match.setData(service->storageId());
 
             addMatch(match);
+            seen.insert(service->storageId());
+            seen.insert(service->exec());
         }
     }
 
@@ -74,6 +82,88 @@ void ApplicationSource::query(const QString& string)
             match.setData(service->storageId());
 
             addMatch(match);
+            seen.insert(service->storageId());
+            seen.insert(service->exec());
+        }
+    }
+
+    // If the term length is < 3, no real point searching the Keywords and GenericName
+    if (term.length() < 3) {
+        queryStr = QString("exist Exec and ( (exist Name and '%1' ~~ Name) or ('%1' ~~ Exec) )").arg(term);
+    } else {
+        // Search for applications which are executable and the term case-insensitive matches any of
+        // * a substring of one of the keywords
+        // * a substring of the GenericName field
+        // * a substring of the Name field
+        // Note that before asking for the content of e.g. Keywords and GenericName we need to ask if
+        // they exist to prevent a tree evaluation error if they are not defined.
+        queryStr = QString("exist Exec and ( (exist Keywords and '%1' ~subin Keywords) or (exist GenericName and '%1' ~~ GenericName) or (exist Name and '%1' ~~ Name) or ('%1' ~~ Exec) )").arg(term);
+    }
+
+    if (m_applicationType->isShown()) {
+        KService::List services = KServiceTypeTrader::self()->query("Application", queryStr);
+        foreach (const KService::Ptr &service, services) {
+            if (service->noDisplay()) {
+                continue;
+            }
+
+            const QString id = service->storageId();
+            const QString name = service->desktopEntryName();
+            const QString exec = service->exec();
+
+            if (seen.contains(id) || seen.contains(exec)) {
+                continue;
+            }
+
+            // If the term was < 3 chars and NOT at the beginning of the App's name or Exec, then
+            // chances are the user doesn't want that app.
+            if (term.size() < 3 && !(name.startsWith(term) || exec.startsWith(term))) {
+                continue;
+            }
+
+            Match match(this);
+            match.setType(m_applicationType);
+            match.setText(service->name());
+            match.setIcon(service->icon());
+            match.setData(service->storageId());
+
+            addMatch(match);
+            seen.insert(service->storageId());
+            seen.insert(service->exec());
+        }
+    }
+
+    if (m_kcmType->isShown()) {
+        KService::List services = KServiceTypeTrader::self()->query("KCModule", queryStr);
+        foreach (const KService::Ptr &service, services) {
+            if (service->noDisplay()) {
+                continue;
+            }
+
+            const QString id = service->storageId();
+            const QString name = service->desktopEntryName();
+            const QString exec = service->exec();
+
+            if (seen.contains(id) || seen.contains(exec)) {
+                //kDebug() << "already seen" << id << exec;
+                continue;
+            }
+
+            // If the term was < 3 chars and NOT at the beginning of the App's name or Exec, then
+            // chances are the user doesn't want that app.
+            if (term.size() < 3 && !(name.startsWith(term) || exec.startsWith(term))) {
+                continue;
+            }
+
+            Match match(this);
+            match.setType(m_kcmType);
+            match.setText(service->name());
+            match.setIcon(service->icon());
+            match.setData(service->storageId());
+
+            addMatch(match);
+            seen.insert(service->storageId());
+            seen.insert(service->exec());
         }
     }
 }
