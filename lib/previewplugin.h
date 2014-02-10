f829634d17e90e46746f3968ea74c0fd447cdd20
Vishesh Handa <me@vhanda.in>
2014-02-10 17:46:05 +0100

    Relicense the library from GPL -> LGPL

--- lib/previewplugin.h
+++ lib/previewplugin.h
@@ -1,27 +1,27 @@
 /*
- * <one line to give the library's name and an idea of what it does.>
- * Copyright 2013  Vishesh Handa <me@vhanda.in>
+ * This file is part of the KDE Milou Project
+ * Copyright (C) 2013  Vishesh Handa <me@vhanda.in>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
-#ifndef PREVIEWPLUGIN_H
-#define PREVIEWPLUGIN_H
+#ifndef MILOU_PREVIEWPLUGIN_H
+#define MILOU_PREVIEWPLUGIN_H
 
 #include <QUrl>
 #include <QDeclarativeItem>
@@ -112,4 +112,4 @@ private:
 K_PLUGIN_FACTORY(factory, registerPlugin<classname>();) \
 K_EXPORT_PLUGIN(factory(#libname))
 
-#endif // PREVIEWPLUGIN_H
+#endif // MILOU_PREVIEWPLUGIN_H
--- lib/qml/qmlplugins.h
+++ lib/qml/qmlplugins.h
@@ -1,24 +1,27 @@
 /*
-    Copyright (C) 2013 Vishesh Handa <me@vhanda.in>
+ * This file is part of the KDE Milou Project
+ * Copyright (C) 2013  Vishesh Handa <me@vhanda.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-
-#ifndef QML_PLUGINS_H
-#define QML_PLUGINS_H
+#ifndef _MILOU_QML_PLUGINS_H
+#define _MILOU_QML_PLUGINS_H
 
 #include <QtDeclarative/QDeclarativeExtensionPlugin>
 
@@ -29,4 +32,4 @@ public:
     virtual void registerTypes(const char *uri);
 };
 
-#endif // QML_PLUGINS_H
+#endif // _MILOU_QML_PLUGINS_H
