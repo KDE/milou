a7f6ae4c4f21d9a25d1af9d2c4d4a426ca8f4cd1
Vishesh Handa <me@vhanda.in>
2014-04-07 17:23:58 +0200

    Add a test to show the model in a widget

---
+++ lib/test/widgettest.cpp
@@ -0,0 +1,74 @@
+/*
+ * This file is part of the KDE Baloo Project
+ * Copyright (C) 2014  Vishesh Handa <me@vhanda.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <QTimer>
+#include <QApplication>
+
+#include <QWidget>
+#include <QListView>
+#include <QHBoxLayout>
+
+#include "../sourcesmodel.h"
+#include "../reversemodel.h"
+
+using namespace Milou;
+
+class TestObject : public QWidget {
+    Q_OBJECT
+public slots:
+    void main();
+
+public:
+    explicit TestObject(QWidget* parent = 0, Qt::WindowFlags f = 0) {
+        QTimer::singleShot(0, this, SLOT(main()));
+    }
+};
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    TestObject obj;
+    obj.show();
+
+    return app.exec();
+}
+
+void TestObject::main()
+{
+    SourcesModel* smodel = new SourcesModel(this);
+    smodel->setQueryLimit(20);
+
+    ReverseModel* rmodel = new ReverseModel(this);
+    rmodel->setSourceModel(smodel);
+
+    QListView* view = new QListView(this);
+    view->setModel(smodel);
+    view->setAlternatingRowColors(true);
+
+    QHBoxLayout* l = new QHBoxLayout(this);
+    l->addWidget(view);
+
+    smodel->setQueryString("fire");
+}
+
+#include "widgettest.moc"
