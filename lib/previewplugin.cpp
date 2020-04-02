KDE/milou

commit 7a6b1a037e65f35d86924910e24ca79ea80b4ae4

Use qmlRegisterAnonymousType
Reviewers: broulik

Reviewed By: broulik

Subscribers: meven, broulik, plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D26489

lib/previewplugin.cpp
@@ -24,6 +24,7 @@
 
 #include <QTextDocument>
 #include <QTextCursor>
+#include <QMimeData>
 
 using namespace Milou;
 
@@ -31,6 +32,11 @@ PreviewPlugin::PreviewPlugin(QObject* parent)
     : QObject(parent)
     , m_context(nullptr)
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+    qmlRegisterAnonymousType<QMimeData>("org.kde.milou", 1);
+#else
+    qmlRegisterType<QMimeData>();
+#endif
 }
 
 PreviewPlugin::~PreviewPlugin()

lib/draghelper.cpp
@@ -29,7 +29,6 @@ using namespace Milou;
 DragHelper::DragHelper(QObject* parent) : QObject(parent)
 , m_dragIconSize(32)
 {
-    qmlRegisterType<QMimeData>("org.kde.milou", 1, 0, "MimeData");
 }
 
 DragHelper::~DragHelper()

commit 92ecad19708db75fb35d912e172f026dc2d41606

Move QMimeData registration back to where it belongs
Fixes regression introduced in 7a6b1a037e65f35d86924910e24ca79ea80b4ae4

BUG: 418109
FIXED-IN: 5.18.5

Differential Revision: https://phabricator.kde.org/D28485

lib/previewplugin.cpp
@@ -24,7 +24,6 @@
 
 #include <QTextDocument>
 #include <QTextCursor>
-#include <QMimeData>
 
 using namespace Milou;
 
@@ -32,11 +31,7 @@ PreviewPlugin::PreviewPlugin(QObject* parent)
     : QObject(parent)
     , m_context(nullptr)
 {
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
-    qmlRegisterAnonymousType<QMimeData>("org.kde.milou", 1);
-#else
-    qmlRegisterType<QMimeData>();
-#endif
+
 }
 
 PreviewPlugin::~PreviewPlugin()

lib/qml/qmlplugins.cpp
@@ -29,6 +29,7 @@
 #include "mousehelper.h"
 
 #include <QQmlEngine>
+#include <QMimeData>
 
 void QmlPlugins::initializeEngine(QQmlEngine *, const char *)
 {
@@ -44,5 +45,10 @@ void QmlPlugins::registerTypes(const char *uri)
                                                   [](QQmlEngine*, QJSEngine*) -> QObject* {
         return new Milou::MouseHelper();
     });
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+    qmlRegisterAnonymousType<QMimeData>(uri, 0);
+#else
+    qmlRegisterType<QMimeData>();
+#endif
 }
 